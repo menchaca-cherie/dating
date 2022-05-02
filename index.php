aa67b7c1cea97fe252c18fccf8a46fffa88a7acf
Cherie Menchaca <menchaca.cherie@student.greenriver.edu>
2022-05-02 00:39:46 -0400

    made changes to the index.php and views pages

--- index.php
+++ index.php
@@ -30,18 +30,38 @@ $f3->route('GET /personal', function(){
 });
 //Define a route for Profile
 $f3->route('POST /profile', function(){
-
+    var_dump($_POST);
+    $_SESSION['first'] = $_POST['first'];
+    $_SESSION['last'] = $_POST['last'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['phone'] = $_POST['phone'];
 
     $view = new Template();
     echo $view->render('views/profile.html');
 });
 $f3->route('POST /interest', function(){
     //echo "Profile";
-
+    var_dump($_POST);
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['gender'] = $_POST['gender'];
     $view = new Template();
     echo $view->render('views/interest.html');
 });
-
+$f3->route('POST /summary', function(){
+    var_dump($_POST);
+    if(empty($_POST['interests']))
+    {
+        $interest = "";
+    }
+    else{
+        $interest = implode(", ", $_POST['interests']);
+    }
+    $_SESSION['interests'] = $interest;
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
 
 //Run fat free
 $f3->run();
