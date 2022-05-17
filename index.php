8d38ee81d49107acc95b3d61a52f921522b4b0f7
Cherie Menchaca <menchaca.cherie@student.greenriver.edu>
2022-05-17 13:07:34 -0400

    modified index.php views

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ session_start();
 
 //Require the autoload File
 require_once ('vendor/autoload.php');
-
+require_once ('model/data-layer.php');
+require_once ('model/validation.php');
 //Create an instance of the base class
 $f3 = Base::instance();
 //echo gettype($f3); example of what type is $f3
@@ -20,31 +21,74 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 //Define a route for Personal Information
-$f3->route('GET /personal', function(){
+$f3->route('GET|POST /personal', function($f3){
     //echo "Personal Information";
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
 
+        //Get the data
+        $first = $_POST['first'];
+        $last = $_POST['last'];
+        $age = $_POST['age'];
+        $phone = $_POST['phone'];
+        $gender = $_POST['gender'];
+        $f3->set('firstName', $first);
+        $f3->set('lastName', $last);
+        $f3->set('age', $age);
+        $f3->set('phone', $phone);
+        $f3->set('userGender', $gender);
+        //if data is valid in first
+//    if(validName($name))
+//    {
+//        $_SESSION['name'] = $_POST['name'];
+//    }
+//    else
+//    {
+//        $f3->set('errors["name"]', 'Please enter your name with alphabets');
+//    }
+    }
+    //Add genders data to hive
+    $f3->set('genders', getGender());
     $view = new Template();
     echo $view->render('views/personal.html');
 });
 //Define a route for Profile
-$f3->route('GET|POST /profile', function(){
+$f3->route('GET|POST /profile', function($f3){
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+        //Get the data
+        $email = $_POST['email'];
+        $state = $_POST['state'];
+        $seeking = $_POST['seeking'];
+        $bio = $_POST['bio'];
+        $f3->set('userState', $state);
+        $f3->set('email', $email);
+        $f3->set('userSeeking', $seeking);
+        $f3->set('bio', $bio);
+    }
 
-    $_SESSION['first'] = $_POST['first'];
-    $_SESSION['last'] = $_POST['last'];
-    $_SESSION['gender'] = $_POST['gender'];
-    $_SESSION['age'] = $_POST['age'];
-    $_SESSION['phone'] = $_POST['phone'];
+    //Add states data to hive
+    $f3->set('states', getState());
+    $f3->set('seekings', getSeeking());
 
     $view = new Template();
     echo $view->render('views/profile.html');
 });
-$f3->route('GET|POST /interest', function(){
+$f3->route('GET|POST /interest', function($f3){
     //echo "Profile";
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+        //Get the data
+        $indoor = $_POST['indoor'];
+        $outdoor = $_POST['outdoor'];
+        $f3->set('interestIndoor', $indoor);
+        $f3->set('interestOutdoor', $outdoor);
 
-    $_SESSION['email'] = $_POST['email'];
-    $_SESSION['state'] = $_POST['state'];
-    $_SESSION['seeking'] = $_POST['seeking'];
-    $_SESSION['bio'] = $_POST['bio'];
+    }
+    //Add states data to hive
+    $f3->set('indoor', getIndoorInterest());
+    $f3->set('outdoor', getOutdoorInterest());
+
 
     $view = new Template();
     echo $view->render('views/interest.html');
